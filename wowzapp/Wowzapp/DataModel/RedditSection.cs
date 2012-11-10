adfc1704804350b99bd541fcf6f38a182a4ab686
Laven <lsukumaran@gmail.com>
2012-11-10 12:17:26 -0500

    Added RedditSection and Item model, and parser that still needs to be tested

---
+++ wowzapp/Wowzapp/DataModel/RedditSection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App2.DataModel
+{
+    public class RedditSection
+    {
+        public string RSS;
+        public string Title;
+        public string Link;
+        public string Description;
+        public SectionImage SecImage;
+        public List<Item> Items;
+
+    }
+    public class SectionImage
+    {
+        public string URL;
+        public string Title;
+        public string Link;
+    }
+
+    public class Item
+    {
+        public string Title;
+        public string Link;
+        public string GUID;
+        public string PubDate;
+        public string Description;
+        public string MediaTitle;
+        public string MediaImage;
+    }
+}
